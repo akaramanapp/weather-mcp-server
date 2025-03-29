1834eb522e21f5dc17d6c7f1cfea0733daf76fc0
abdulkerim.karaman <abdulkerim.karaman@albarakatech.com>
2025-03-29 13:54:28 +0300

    add fetch lib.

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
 import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
 import { z } from "zod";
+const fetch = require('node-fetch');
 
 const NWS_API_BASE = "https://api.weather.gov";
 const USER_AGENT = "weather-app/1.0";
